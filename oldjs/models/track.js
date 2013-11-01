605271b171847ba1dc1d6c6e86c835100d2a163e
nicodel <nicolas.delebecque@gmail.com>
2013-11-01 16:57:40 +0100

    adding old js as templates

---
+++ oldjs/models/track.js
@@ -0,0 +1,15 @@
+var TrackModel = function(){
+  var current_track;
+  function init(){}
+  function create(){}
+  function addNode(inNode){}
+  function save(){}
+  
+
+  return {
+    init: init,
+    create: create,
+    addNode: addNode,
+    save: save
+  };
+}();
