72561c8c2d9556702b099b87c276abb5dbf01396
nicodel <nicolas.delebecque@gmail.com>
2014-03-14 15:28:31 +0100

    Update install.js

--- js/install.js
+++ js/install.js
@@ -1,12 +1,12 @@
 // install.js
 var Install = function() {
   console.log("install api");
-  var manifest_url = "http://nicodel.github.com/Run-Bike-Hike/manifest.webapp";
+  var manifest_url = "http://nicodel.github.io/Run-Bike-Hike/manifest.webapp";
   
   function __install(ev) {
     console.log("installing !");
     ev.preventDefault();
-    var installApp = navigator.mozApps.install(manifest_url);
+    var installApp = navigator.mozApps.install("http://nicodel.github.io/Run-Bike-Hike/manifest.webapp");
 
     installApp.onsuccess = function(data) {};
 
@@ -16,7 +16,7 @@ var Install = function() {
   }
   
   var button = document.getElementById("install");
-  var installCheck = navigator.mozApps.checkInstalled(manifest_url);
+  var installCheck = navigator.mozApps.checkInstalled("http://nicodel.github.io/Run-Bike-Hike/manifest.webapp");
   installCheck.onsuccess = function() {
     console.log("checking app installation");
     if(installCheck.result) {
