1c3a7904e110d75c887851007f8acbc7d9d5f9ad
nicodel <nicolas.delebecque@gmail.com>
2014-07-05 23:37:50 +0200

    bricks, correcting installation

--- js/base.js
+++ js/base.js
@@ -15,7 +15,7 @@ if (navigator.mozApps) {
         }
         else {
             var install = document.querySelector("#install"),
-                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/Run-Bike-App/")) + "/manifest.webapp";
             install.className = "show-install";
             install.onclick = function () {
                 var installApp = navigator.mozApps.install(manifestURL);
