2bb653fd78c105079411e8f62a08d0cd33cdcce2
del <del@localhost.localdomain>
2014-07-17 12:06:43 +0200

    all, changing to usb drive

---
+++ js/models/db_proto.js
@@ -0,0 +1,48 @@
+var DATABASE = function() {
+  
+  var DB_NAME,        // database name
+      DB_VERSION,     // database intial version
+      STORE_NAME,     // 
+      STORE_KEYPATH,
+      STORE_ID,
+      STORE_INDEX,
+      STORE_STRUCT;
+
+  function initiate(successCallback, errorCallback) {
+    if (typeof(successCallback) === "function") {
+      var req = window.indexedDB.open(DB_NAME, DB_VERSION);
+      req.onsuccess = function(e) {
+        successCallback(req.result);
+        db = req.result;
+        db.onabort = function(e) {
+          db.close();
+          db = null;
+        };
+      };
+      req.onerror = function(e) {
+        errorCallback(e.target.error.name);
+        g_error = true;
+      };
+      req.onupgradeneeded = function(event) {
+        var store = req.result.createObjectStore(STORE_NAME, {keyPath:STORE_KEYPATH, autoIncrement: true});
+        store.createIndex("trackid", "trackid", {unique: true});
+      };
+    } else  {
+      errorCallback("initiate() successCallback should be a function");
+    }
+  }
+
+  function addStore() {}
+  function del() {}
+  function getAll() {}
+  function reset() {}
+
+  return {
+    initiate: initiate,
+    addStore: addStore,
+    getAll: getAll,
+    del: del,
+    reset: reset,
+    DB_NAME: DB_NAME
+  }
+}();
---
+++ js/views/infos-view.js
@@ -0,0 +1,87 @@
+var InfosView = function() {
+/*define(["controller",
+        "models/config"
+  ], function(Controller, Config) {*/
+
+  var updateInfos = function(inPosition, inDistance) {
+    console.log("showing: ", inDistance);
+    // updating distance using Settings choosen unit
+    document.getElementById("infos-dist").innerHTML = Controller.userDistance(inDistance);
+    // updating speed using Settings choosen unit
+    document.getElementById("infos-speed").innerHTML = Controller.userSpeed(inPosition.coords.speed);
+    // updating altitude using Settings choosen unit
+    document.getElementById("infos-alt").innerHTML = Controller.userSmallDistance(inPosition.coords.altitude);
+    // updating accuracy using settings units
+    document.getElementById("infos-acc").innerHTML = "&#177;" + Controller.userSmallDistance(inPosition.coords.accuracy);
+    // checking accuracy and display appropriate GPS status
+    if (inPosition.coords.accuracy > 30) {
+      document.getElementById("infos-acc").className = "align-right bold bad-signal";
+    } else {
+      document.getElementById("infos-acc").className = "align-right bold";
+    }
+    // update compass direction
+    __displayCompass(inPosition.coords);
+  }
+
+  var __displayCompass = function(event) {
+
+    compass = document.getElementById("infos-compass");
+    //~ console.log("heading:", event.heading);
+    if (event.heading > 0 ){
+      /** in case, when GPS is disabled (only if GSM fix is available),
+       * event.heading should be -1 and event.errorCode should be 4,
+       * but it isn't... So we use this strange condition that don't
+       * work if we go _directly_ to north...
+       */
+      opacity = 1; // 0.8
+      compass.src = 'img/compass.png';
+      var rot = 360 - event.heading.toFixed(0);
+      compass.style.transform = "rotate(" + rot + "deg)";
+      // compass.style.webkitTransform = "rotate(" + rot + "deg)";
+    } else {
+      compass.src = 'img/compass_inactive.png';
+      opacity = 1; // 0.3
+    }
+    compass.style.opacity = opacity;
+  }
+  /*
+  var cent=0;
+  var sec=0;
+  var min = 0;
+  var compte;
+
+  var startChrono = function() {
+    cent++;
+    if (cent > 9) {cent = 0;sec++;}
+    if (sec > 59) {sec = 0;min++;}
+
+    if (sec < 10) {document.getElementById('trk-sec').innerHTML = '0' + sec;}
+    else {document.getElementById('trk-sec').innerHTML = sec;}
+
+    if (min < 10)
+      {document.getElementById('trk-min').innerHTML = '0' + min;}
+    else
+      {document.getElementById('trk-min').innerHTML = min;}
+    compte = window.setTimeout(startChrono, 100);
+  }
+
+  var stopChrono = function() {
+    window.clearTimeout(compte);
+    cent = 0;
+    sec = 0;
+    min = 0;
+    // document.getElementById('dur-min').innerHTML = min;
+    // document.getElementById('dur-sec').innerHTML = sec;
+  }*/
+
+
+
+  return {
+    /*startChrono: startChrono,
+    stopChrono:stopChrono,*/
+    updateInfos: updateInfos
+    // backHome: backHome
+  }
+
+}();
+// });
