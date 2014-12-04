085e76de97ec05d3b717bedd3d7f6b1e139e8fc5
Bonbadil <bonbadil@yahoo.fr>
2014-12-04 23:21:07 +0100

    Import: disable confirm button each time, allow any caracter in file name and load multiple segments in track files.

--- js/views/import-view.js
+++ js/views/import-view.js
@@ -1,14 +1,10 @@
 var importView = function() {
 
   function addFile(inFile) {
-    
     var s = document.getElementById("select-file");
-
-    console.log("adding file to DOM", inFile.name.match(/[-_\w]+[.][\w]+$/i)[0]);
-    
     var o = document.createElement("option");
     o.value = inFile.name;
-    o.innerHTML = inFile.name.match(/[-_\w]+[.][\w]+$/i)[0];
+    o.innerHTML = inFile.name.match(/[^/]+$/i)[0];
     s.appendChild(o);
   }
   function resetList() {
