f1e069672e8fd8af3bcdf60570fec7b17a5a4984
nicodel <nicolas.delebecque@gmail.com>
2014-12-15 16:31:13 +0100

    working on casperjs testing

---
+++ casperjs/test-config.js
@@ -0,0 +1,44 @@
+/* jshint browser: true, devel: true, strict: true, phantom: true */
+/* global casper */
+// casperjs test test-config.js --includes=../js/models/config.js --verbose
+var require = patchRequire(require);
+var config  = require("../js/models/config.js");
+var DEFAULT_CONFIG = [
+  {screen: false},
+  {language: "en"},
+  {distance: "0"},
+  {speed: "0"},
+  {position: "0"},
+  {frequency: "auto"}
+];
+casper.test.begin("Testing Config", 9, function suite (test) {
+  "use strict";
+  for (var i = 0; i < DEFAULT_CONFIG.length; i++) {
+    var param = DEFAULT_CONFIG[i];
+    if (param === "screen") {
+      Config.change("SCREEN_KEEP_ALIVE", DEFAULT_CONFIG[param]);
+    } else if (param === "distance") {
+      Config.change("USER_DISTANCE", DEFAULT_CONFIG[param]);
+    } else if (param === "speed") {
+      Config.change("USER_SPEED", DEFAULT_CONFIG[param]);
+    } else if (param === "position") {
+      Config.change("USER_POSITION_FORMAT", DEFAULT_CONFIG[param]);
+    }
+  }
+  Config.CONFIG = DEFAULT_CONFIG;
+  test.info("Config.userSpeed");
+  var cases = [
+    [null, "--"],
+    [-6, "--"],
+    [NaN, "--"],
+    [Infinity, "--"],
+    [100, 360]
+  ];
+  for (var i = 0; i < cases.length; i++) {
+    var cas = cases[i]
+    var speed = Config.userSpeed(cas[0]);
+    test.assertEquals(speed.v, cas[1], "for " + cas[0]);
+  }
+  
+  test.done();
+});
