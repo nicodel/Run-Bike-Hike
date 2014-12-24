60750d678209705a4e9f10d28d93bbe45bd880c4
nicodel <nicolas.delebecque@gmail.com>
2014-12-24 13:20:10 +0100

    I will completely re-ingineer the whole sd-card.js, share.js, export-track.js, because it is a fu***g mess

---
+++ js/models/storage.js
@@ -0,0 +1,58 @@
+/* jshint browser: true, devel: true, strict: true */
+/* exported Storage */
+/* global _ */
+var Storage = function() {
+  "use strict";
+  var user_storage;
+
+  function getAvailable() {
+    var storages = navigator.getDeviceStorages("sdcard");
+    var available_storages;
+    storages.forEach(function(sto) {
+      available_storages.push(sto.storageName);
+    });
+    return available_storages;
+  }
+
+  function searchForFiles(inPath, inType, successCallback, errorCallback) {
+    if (typeof(successCallback) === "function") {
+      var s = navigator.getDeviceStorages("sdcard")[user_storage];
+      var req = s.enumerate(inPath);
+      req.onsuccess = function() {
+        if(this.result) {
+          var file = this.result;
+          var type = "." + inType;
+          if (file.name.match(/\.[0-9a-z]+$/i).toString() === type) {
+            console.log("just got file:", file);
+            successCallback(file);
+          }
+          this.continue();
+        }
+      };
+      req.onerror = function(e) {
+        if (e.target.error.name === "NotFoundError") {
+          errorCallback(_("import-missing"));
+        }
+      };
+    } else {
+      errorCallback("searchForFile() successCallbak should be a function");
+    }
+  }
+
+  function getFile() {}
+
+  function saveFile() {}
+
+  function setUserStorage(inStorage) {
+    user_storage = inStorage;
+  }
+
+  return {
+    getAvailable:   getAvailable,
+    searchForFiles: searchForFiles,
+    getFile:        getFile,
+    saveFile:       saveFile,
+    setUserStorage: setUserStorage
+  };
+
+}();
