87aea2f077a64a4663556da476248181aa36b1c6
Bonbadil <bonbadil@yahoo.fr>
2015-01-09 21:09:09 +0100

    Fix generated names and duration of imported tracks.

--- js/models/tracks.js
+++ js/models/tracks.js
@@ -33,13 +33,13 @@ var Tracks = function() {
       day = "0" + day.toString();
     }
     if (hour < 10) {
-      hour = "0" + day.toString();
+      hour = "0" + hour.toString();
     }
     if (min < 10) {
-      min = "0" + day.toString();
+      min = "0" + min.toString();
     }
     if (sec < 10) {
-      sec = "0" + day.toString();
+      sec = "0" + sec.toString();
     }
 
     current_track.name = "TR-"+year+month+day+"-"+hour+min+sec;
