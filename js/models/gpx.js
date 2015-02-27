ea5f59234d79c12f615665a59f68cd17f44b25cb
nicodel <nicolas.delebecque@gmail.com>
2015-02-26 19:19:52 -0500

    replacing single quotes by double quotes when exporting a track to gpx file

--- js/models/gpx.js
+++ js/models/gpx.js
@@ -2,16 +2,8 @@
 /* exported GPX */
 
 var GPX = function() {
-  "use strict";
+  'use strict';
 
-/*  var T = {
-    id: new Date().toISOString(),
-    name: null,
-    duration: 0,
-    distance: 0,
-    map: "",
-    data: []
-  };*/
   var olat = null;
   var olon = null;
   var distance = 0;
@@ -20,52 +12,12 @@ var GPX = function() {
     olat = null;
     olon = null;
     distance = 0;
-    // var n = "rbh/import/" + inFile.name.match(/[^/]+$/i)[0];
-/*    var reader = new FileReader(); 
-    reader.onloadend = function() {
-      var p = new DOMParser();
-      __parse(p.parseFromString(reader.result, "text/xml"),
-          successCallback,
-          failureCallback);
-    };
-    reader.onerror = function(e) {
-      console.log("reader error:", e);
-      failureCallback(_("error-reading-file",
-            {file:inFile.name.match(/[^/]+$/i)[0],
-              error:e.target.result}
-            ));
-    };
-    reader.readAsText(inFile);*/
+    // var n = 'rbh/import/' + inFile.name.match(/[^/]+$/i)[0];
     __parse(inFile,
         successCallback,
         failureCallback);
 
   };
-
-/*  var verify = function(inFile, successCallback, failureCallback) {
-    // var n = "rbh/import/" + inFile.name.match(/[^/]+$/i)[0];
-    var reader = new FileReader(); 
-    reader.onloadend = function() {
-      // console.log("reader success", reader.result);
-      var p = new DOMParser();
-      var x = p.parseFromString(reader.result, "text/xml");
-
-      // var metadata = x.getElementsByTagName("metadata");
-      // var time = metadata[0].getElementsByTagName("time");
-      var trk = x.getElementsByTagName("trk");
-      if (trk.length > 0) {
-        successCallback(inFile);
-        // t = trk[0];
-      } else {
-        failureCallback(_("no-track-in-file", {file:inFile.name.match(/[^/]+$/i)[0]}));
-      }
-    };
-    reader.onerror = function(e) {
-      console.log("reader error:", e);
-      failureCallback(_("error-reading-file", {file:inFile.name.match(/[^/]+$/i)[0], error:e.target.result}));
-    };
-    reader.readAsText(inFile);
-  };*/
 
   var __parse = function(x, successCallback, failureCallback) {
     var track = {
@@ -73,15 +25,15 @@ var GPX = function() {
       name: null,
       duration: 0,
       distance: 0,
-      map: "",
-      date: "",
+      map: '',
+      date: '',
       data: []
     };
     var missing_time,
         tstart,
         tend;
-    var metadata = x.getElementsByTagName("metadata");
-    var time = metadata[0].getElementsByTagName("time");
+    var metadata = x.getElementsByTagName('metadata');
+    var time = metadata[0].getElementsByTagName('time');
     if (time.length > 0) {
       track.date = time[0].textContent;
     } else {
@@ -89,33 +41,33 @@ var GPX = function() {
     }
 
     var t;
-    var trk = x.getElementsByTagName("trk");
+    var trk = x.getElementsByTagName('trk');
     if (trk.length > 0) {
       t = trk[0];
     } else {
-      failureCallback("no track found in loaded file");
+      failureCallback('no track found in loaded file');
     }
 
-    var name = t.getElementsByTagName("name");
+    var name = t.getElementsByTagName('name');
     if (name.length > 0) {
       track.name = name[0].textContent;
     } else {
       track.name = __named();
     }
 
-    var trkseg = t.getElementsByTagName("trkseg");
+    var trkseg = t.getElementsByTagName('trkseg');
     var trkpt, tag;
     if (trkseg.length > 0) {
       for (var i = 0; i < trkseg.length; i++) {
-        trkpt = trkseg[i].getElementsByTagName("trkpt");
+        trkpt = trkseg[i].getElementsByTagName('trkpt');
         if (trkpt.length > 0) {
           for (var j = 0; j < trkpt.length; j++) {
             var point = {};
             var p = trkpt[j];
-            point.latitude = p.getAttribute("lat");
-            point.longitude = p.getAttribute("lon");
+            point.latitude = p.getAttribute('lat');
+            point.longitude = p.getAttribute('lon');
             distance = __getDistance(point.latitude, point.longitude);
-            tag = p.getElementsByTagName("time");
+            tag = p.getElementsByTagName('time');
             if (tag.length > 0) {
               point.date = tag[0].textContent;
               if (missing_time) {
@@ -129,39 +81,39 @@ var GPX = function() {
             } else {
               track.date = 0;
             }
-            tag = p.getElementsByTagName("ele");
+            tag = p.getElementsByTagName('ele');
             if (tag.length > 0) {
               point.altitude = tag[0].textContent;
             }
 
-            tag = p.getElementsByTagName("speed");
+            tag = p.getElementsByTagName('speed');
             if (tag.length > 0) {
               point.speed = tag[0].textContent;
             }
 
-            tag = p.getElementsByTagName("time");
+            tag = p.getElementsByTagName('time');
             if (tag.length > 0) {
               point.date = tag[0].textContent;
             }
 
-            tag = p.getElementsByTagName("hdop");
+            tag = p.getElementsByTagName('hdop');
             if (tag.length > 0) {
               point.accuracy = tag[0].textContent;
             }
 
-            tag = p.getElementsByTagName("vdhop");
+            tag = p.getElementsByTagName('vdhop');
             if (tag.length > 0) {
               point.vertAccuracy = tag[0].textContent;
             }
-            // console.log("point", point);
+            // console.log('point', point);
             track.data.push(point);
           }
         } else {
-          failureCallback("Could not parse trkpt from file");
+          failureCallback('Could not parse trkpt from file');
         }
       }
     } else {
-      failureCallback("Could not parse track segment from file");
+      failureCallback('Could not parse track segment from file');
     }
     if (tend && tstart) {
       track.duration = tend - tstart;
@@ -182,85 +134,85 @@ var GPX = function() {
     var min = d.getMinutes();
     var sec = d.getSeconds();
     if (month < 10) {
-      month = "0" + month.toString();
+      month = '0' + month.toString();
     }
     if (day < 10) {
-      day = "0" + day.toString();
+      day = '0' + day.toString();
     }
     if (hour < 10) {
-      hour = "0" + hour.toString();
+      hour = '0' + hour.toString();
     }
     if (min < 10) {
-      min = "0" + min.toString();
+      min = '0' + min.toString();
     }
     if (sec < 10) {
-      sec = "0" + sec.toString();
+      sec = '0' + sec.toString();
     }
 
-    return "TR-"+year+month+day+"-"+hour+min+sec;
+    return 'TR-'+year+month+day+'-'+hour+min+sec;
   };
 
   var __getDistance = function(lat, lon) {
-    // console.log("__getDistance");
-    // console.log("olat", olat);
+    // console.log('__getDistance');
+    // console.log('olat', olat);
     if (olat !== null) {
       distance += __distanceFromPrev(olat, olon, lat, lon);
     }
     olat = lat;
     olon = lon;
-    // console.log("calc distance: ", distance);
+    // console.log('calc distance: ', distance);
     return distance;
   };
 
   var __distanceFromPrev = function(lat1, lon1, lat2, lon2) {
-    // console.log("__getDistanceFromPrev");
+    // console.log('__getDistanceFromPrev');
     var lat1Rad = lat1*( Math.PI / 180);
-    // console.log("lat1Rad: ", lat1Rad);
+    // console.log('lat1Rad: ', lat1Rad);
     var lon1Rad = lon1*( Math.PI / 180);
     var lat2Rad = lat2*( Math.PI / 180);
     var lon2Rad = lon2*( Math.PI / 180);
 
     var dLat = lat2Rad - lat1Rad;
-    // console.log("dLat: ", dLat);
+    // console.log('dLat: ', dLat);
     var dLon = lon2Rad - lon1Rad;
 
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(dLon/2) * Math.sin(dLon/2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var R = 6371 * 1000; // Earth radius (mean) in metres {6371, 6367}
-    // console.log("R*c: ", R*c);
+    // console.log('R*c: ', R*c);
     return R * c;
   };
 
 
   var create = function(inTrack) {
-    var name = inTrack.name.replace(/&/g,"&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    // console.log("exporting ", name);
-    var data = "";
-    data += "<?xml version='1.0' encoding='UTF-8'?>\n";
-    data += "<gpx version='1.1'\n";
-    data += "creator='Run, Bike, Hike - https://github.com/nicodel/Run-Bike-Hike'\n";
-    data += "xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'\n";
-    data += "xmlns='http://www.topografix.com/GPX/1/1'\n";
-    data += "xsi:schemaLocation='http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd'>\n";
-    data += "<metadata>\n";
-    data += "<author><name>Nicolas Delebecque</name><link href='https://github.com/nicodel/'></link></author>";
-    data += "<name>" + name + "</name>";
-    data += "<time>" + new Date().toISOString() + "</time>";
-    data += "</metadata>";
-    data += "<trk>\n<name>" + name + "</name>\n<trkseg>\n";
+    var name = inTrack.name.replace(/&/g,'&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    // console.log('exporting ', name);
+    var data = '';
+    data += '<?xml version="1.0" encoding="UTF-8"?>\n';
+    data += '<gpx version="1.1"\n';
+    data += 'creator="Run, Bike, Hike - https://github.com/nicodel/Run-Bike-Hike"\n';
+    data += 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n';
+    data += 'xmlns="http://www.topografix.com/GPX/1/1"\n';
+    data += 'xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">\n';
+    data += '<metadata>\n';
+    data += '<author><name>Nicolas Delebecque</name><link href="https://github.com/nicodel/"></link></author>';
+    data += '<name>' + name + '</name>';
+    data += '<time>' + new Date().toISOString() + '</time>';
+    data += '</metadata>';
+    data += '<trk>\n<name>' + name + '</name>\n<trkseg>\n';
     for (var i = 0; i < inTrack.data.length; i++) {
       var row = inTrack.data[i];
-      data += "<trkpt lat='" + row.latitude + "' lon='" + row.longitude + "'>\n";
-      data += "\t<time>" + row.date + "</time>\n";
-      data += ((row.altitude) && (row.altitude !== "null"))?"\t<ele>" + row.altitude + "</ele>\n" : "";
-      data += (row.speed>=0) ? "\t<speed>" +row.speed+ "</speed>\n" : "";
-      data += (row.accuracy>0)?"\t<hdop>" + row.accuracy + "</hdop>\n" : "";
-      data += (row.vertAccuracy>0)?"\t<vdop>" + row.vertAccuracy + "</vdop>\n" : "";
-      data += "</trkpt>\n";
+      data += '<trkpt lat="' + row.latitude + '" lon="' + row.longitude + '">\n';
+      data += '\t<time>' + row.date + '</time>\n';
+      data += ((row.altitude) && (row.altitude !== 'null'))?'\t<ele>' + row.altitude + '</ele>\n' : '';
+      data += (row.speed>=0) ? '\t<speed>' +row.speed+ '</speed>\n' : '';
+      data += (row.accuracy>0)?'\t<hdop>' + row.accuracy + '</hdop>\n' : '';
+      data += (row.vertAccuracy>0)?'\t<vdop>' + row.vertAccuracy + '</vdop>\n' : '';
+      data += '</trkpt>\n';
     }
-    data += "</trkseg>\n</trk>\n";
-    data += "</gpx>\n";
-    // console.log("export done", data);
+    data += '</trkseg>\n</trk>\n';
+    data += '</gpx>\n';
+    // console.log('export done', data);
     return data;
   };
 
