237b5c2606e1d481d7ca64447711cc7aeaf3a9ea
nicodel <nicolas.delebecque@gmail.com>
2015-03-23 16:10:03 +0100

    cleaning js/app.js

--- js/app.js
+++ js/app.js
@@ -11,15 +11,4 @@ var RunBikeHike = function() {
     var _ = document.webL10n.get;
     Controller.init();
   });
-  
-  /*if (typeof String.prototype.startsWith != 'function') {
-    String.prototype.startsWith = function (str){
-      return this.slice(0, str.length) == str;
-    };
-  };
-  if (typeof String.prototype.endsWith != 'function') {
-    String.prototype.endsWith = function (str){
-      return this.slice(-str.length) == str;
-    };
-  };*/
 }();
