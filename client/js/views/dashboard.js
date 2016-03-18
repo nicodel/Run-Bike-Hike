672bf42771f6140367757e6b657e4d8e7e7a3511
nicodel <nicolas@nicodel.com>
2016-03-18 21:20:12 +0100

    cleaning unnecessary console.log

--- client/js/views/dashboard.js
+++ client/js/views/dashboard.js
@@ -64,7 +64,6 @@ var DashboardView = Backbone.NativeView.extend({
   },
 
   negateString: function(s) {
-    console.log('s is', s);
     s = s.toLowerCase();
     s = s.split("");
     s = s.map(function(letter) {
